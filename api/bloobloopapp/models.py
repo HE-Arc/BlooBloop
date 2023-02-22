cbb955ff456e08123a515ceb8dd94406f8f601d4
!+VinZ+! <jeannin.vincent@gmail.com>
2023-02-22 13:56:31 +0100

    dev(Conversation) : #3 done - Create ConversationItem model

--- api/bloobloopapp/models.py
+++ api/bloobloopapp/models.py
@@ -1,14 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class MessageItem(models.Model):
-    user = models.ForeignKey(
-        User, related_name="message_items", on_delete=models.SET_NULL
-    )
-    conversation = models.ForeignKey(
-        Conversation, related_name="message_items", on_delete=models.CASCADE
-    )
-    content = models.CharField(max_length=2048)
+
+class ConversationItem(models.Model):
+    name = models.CharField(max_length=50)
+    users = models.ManyToManyField(User)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
