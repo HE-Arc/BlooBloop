2a320f21dd7ca5496fd5d2435d432136985c8754
Filippos Gleglakos <filippos.gleglakos@he-arc.ch>
2023-02-22 13:47:51 +0100

    #3 Added the message serializer containing the serializable fields.

--- api/bloobloopapp/serializers.py
+++ api/bloobloopapp/serializers.py
@@ -1,54 +1,17 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import ConversationItem
+from django.contrib.auth.models import User
+from .models import MessageItem
 
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                        USER Serializer                        #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class MessageItemSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = User
+        model = MessageItem
         fields = [
             "url",
             "id",
-            "username",
+            "user",
+            "conversation",
+            "content",
+            "created_at",
+            "updated_at",
         ]
-
-
-class ComplexUserSerializer(UserSerializer):
-    conversations = serializers.HyperlinkedRelatedField(
-        many=True, view_name="conversation-detail", read_only=True
-    )
-
-    class Meta:
-        model = User
-        fields = UserSerializer.Meta.fields + ["conversations"]
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                    CONVERSATION Serializer                    #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-
-class ConversationItemSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = ConversationItem
-        fields = [
-            "url",
-            "id",
-            "name",
-            "users",
-        ]
-
-
-class ComplexConversationItemSerializer(ConversationItemSerializer):
-    users_objects = serializers.HyperlinkedRelatedField(
-        many=True, view_name="user_detail", read_only=True
-    )
-
-    class Meta:
-        model = ConversationItem
-        fields = ConversationItemSerializer.Meta.fields + ["users_objects"]
