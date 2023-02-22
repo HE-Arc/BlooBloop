9cb41c24f1b412bcbd1d9ccf858726c4fe839765
Filippos Gleglakos <filippos.gleglakos@he-arc.ch>
2023-02-22 13:29:04 +0100

    #2 Added the MessageItem model.

--- api/bloobloopapp/models.py
+++ api/bloobloopapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class MessageItem(models.Model):
+    user = models.ForeignKey(
+        User, related_name="message_items", on_delete=models.SET_NULL
+    )
+    conversation = models.ForeignKey(
+        Conversation, related_name="message_items", on_delete=models.CASCADE
+    )
+    content = models.CharField(max_length=2048)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
