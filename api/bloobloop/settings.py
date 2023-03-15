a42d1ec38a0754e32d85e7c96d97f72bcdafe1e0
!+VinZ+! <jeannin.vincent@gmail.com>
2023-03-15 17:37:43 +0100

    Move .env and update settings

--- api/bloobloop/settings.py
+++ api/bloobloop/settings.py
@@ -22,7 +22,7 @@ env = environ.Env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR.parent, ".env"))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
