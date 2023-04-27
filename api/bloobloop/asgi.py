d3449a05ed2f5060e7feb8a7203a19fc085a350e
Théo Vuilliomenet <theo.vuilliomenet@he-arc.ch>
2023-04-27 15:35:33 +0200

    Add dynamic websocket security

--- api/bloobloop/asgi.py
+++ api/bloobloop/asgi.py
@@ -8,17 +8,21 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-import bloobloop.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bloobloop.settings")
 
+django_asgi_app = get_asgi_application()
+
+import bloobloop.routing
+
 application = ProtocolTypeRouter(
     {
-        "https": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(bloobloop.routing.websocket_urlpatterns))
         ),
--- frontend/src/views/Conversations-index.vue
+++ frontend/src/views/Conversations-index.vue
@@ -7,11 +7,11 @@ import moment from "moment";
 import "@/assets/shake.css";
 
 const API_URL = import.meta.env.VITE_API_URL;
-const WS_HOST = import.meta.env.VITE_WEBSOCKET_HOST;
+//const WS_HOST = import.meta.env.VITE_WEBSOCKET_HOST;
 
 const route = useRoute();
 const router = useRouter();
-const errors = ref(null);
+//const errors = ref(null);
 
 const loggedUserId = ref(-1);
 const message = ref("");
@@ -42,8 +42,11 @@ const fetchMessages = async (id) => {
 };
 
 const startWebSocket = async (id) => {
-  console.log("Hello there");
-  webSocket = new WebSocket("wss://" + WS_HOST + "/ws/chat/" + id + "/");
+  var ws_scheme = window.location.protocol === "https:" ? "wss" : "ws";
+
+  webSocket = new WebSocket(
+    ws_scheme + "://" + window.location.host + "/ws/chat/" + id + "/"
+  );
 
   // receive message
   webSocket.onmessage = () => {
